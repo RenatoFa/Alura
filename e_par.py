7fb9643822853dcdb37871516068e509c5e381d0
Renato <renato.v6@hotmail.com>
2021-01-27 17:07:09 -0300

    Add: palavras.txt

---
+++ e_par.py
@@ -0,0 +1,6 @@
+inteiros = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# list comprehension
+par = [x for x in inteiros if(x % 2 == 0)]
+
+print(par)
--- forca.py
+++ forca.py
@@ -1,31 +1,46 @@
 def jogar():
+
     print("********************************")
     print("Bem vindo no jogo da Forca")
     print("********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
         # funcao .strip() = retira o espaço do input ou de uma string
         # funcao .upper() = passa a string para letras maiusculas
         chute = (input("Qual é a letra? ")).strip().upper()
-
         # find nao vai funcionar , pois existe letras repetidas
         # vamos implementar um for para pecorrer a palavra
+        # A tupla é uma sequecia tambem ( igual a lista) , so que ela é imutavel.
 
-        index = 0
-        for letra in palavra_secreta:
-            if chute == letra.upper():
-                letras_acertadas[index] = letra
-            index += 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra.upper()):
+                    letras_acertadas[index] = letra
+                index += 1
+
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativa.".format(6-erros))
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
-        print("jogando...")
+        if(acertou):
+            print("Você ganhou!!")
+        else:
+            print("Tem que ganhar pow!!! Tenta mais uma vez ai.... ")
 
 
 if(__name__ == "__main__"):
